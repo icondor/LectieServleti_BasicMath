a1daa55b85032c9813e2273b8c93222c6bbbb686
Ionel Condor <ionel-condor@yahoo.com>
2015-08-05 19:23:05 +0300

    Initial commit

---
+++ src/main/java/OperatiiMatematiceAjax.java
@@ -0,0 +1,42 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+public class OperatiiMatematiceAjax extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        PrintWriter out =resp.getWriter();
+
+        String sNr1=req.getParameter("nr1");
+        String sNr2=req.getParameter("nr2");
+        String sOp=req.getParameter("op");
+
+        int nr1=Integer.parseInt(sNr1);
+        int nr2=Integer.parseInt(sNr2);
+        double resultValue=0;
+
+        if(sOp.equals("1"))
+            resultValue=nr1+nr2;
+      else
+        if(sOp.equals("2"))
+            resultValue=nr1-nr2;
+        else
+        if(sOp.equals("3"))
+            resultValue=nr1*nr2;
+        else
+        if(sOp.equals("4"))
+            resultValue=(double)nr1/nr2;
+
+
+
+        out.println("rezultatul este: <b>"+resultValue+"</b>");
+
+        out.close();
+
+    }
+}
